2d1dc4fd7c26f89ca55feda01508b36a10b69589
romain.causse.altran <romain.causse@altran.com>
2021-01-31 20:41:41 +0100

    fix constructor defaut

--- src/app/models/serie.ts
+++ src/app/models/serie.ts
@@ -2,12 +2,12 @@ import {Comment} from './comment';
 
 export class Serie {
   constructor(
-    public  id: number,
-    public name: string,
-    public firstReleaseDate: string,
-    public seasonsNumber: number,
-    public description: string,
-    public  review: string,
+    public id: number = null,
+    public name: string = '',
+    public firstReleaseDate: string = '',
+    public seasonsNumber: number = 0,
+    public description: string = '',
+    public  review: string = '',
     public photo?: string,
     public comments?: Comment[]
   ) {
